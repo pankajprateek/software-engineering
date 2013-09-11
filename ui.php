7a4435d69d288025736e0684c843325b4442cb9b
rishabhnigam31 <rishabhnigam31@gmail.com>
2013-09-11 22:54:20 +0530

    initial ui using twitter bootstrap

---
+++ ui.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset = "utf8">
+	<title>Cloud Storage Simplified</title>
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.css"  type="text/css"/>
+	<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+	<script src="bootstrap/js/bootstrap.js"></script>
+</head>
+<body>
+
+<div class = "container">	
+	<h1><a href="#"> Cloud Storage Simplified </a></h1> 
+	<table class = "table table-hover"
+		<thead>
+			<tr>
+				<td>		Service		</td>
+				<td>		Connected		</td>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td> <a href = "drive.php"> <img src="drive.jpg" height = "75px" width = "75px"/> </a>	</td>
+				<td> Not </td>	
+			</tr>
+			<tr>
+			<td> <a href = "dropbox.php"> <img src="dropbox.png" height = "75px" width = "75px"/> </a>	</td>
+			<td> Not </td>	
+			</tr>
+		</tbody>
+	</div>
+</div>
+</body>
+</html>
