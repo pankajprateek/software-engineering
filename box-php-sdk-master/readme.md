af448d7f20c15a7281cf94e7d0d5442d2637b8f1
pankajprateek <pankaj200292@gmail.com>
2013-09-11 16:45:43 +0530

    box auth working

---
+++ box-php-sdk-master/readme.md
@@ -0,0 +1,1 @@
+# This SDK has been deprecated. Please use the V2 API: developers.box.com
---
+++ box.php
@@ -0,0 +1,28 @@
+<?php 
+// Include the Box_Rest_Client class
+include('box-php-sdk-master/lib/Box_Rest_Client.php');
+                      
+// Set your API Key. If you have a lot of pages reliant on the 
+// api key, then you should just set it statically in the 
+// Box_Rest_Client class.
+$api_key = '86slra9tek0b1gccb9iwy1ualyc2qqk5';
+$box_net = new Box_Rest_Client($api_key);
+          
+
+
+
+if(!array_key_exists('auth',$_SESSION) || empty($_SESSION['auth'])) {
+  $_SESSION['auth'] = $box_net->authenticate();
+  echo $_SESSION['auth']."<br/>";
+}
+else {
+  // If the auth $_SESSION key exists, then we can say that 
+  // the user is logged in. So we set the auth_token in 
+  // box_net.
+  $box_net->auth_token = $_SESSION['auth'];
+  echo $_SESSION['auth']."<br/>";
+} 
+echo $box_net->auth_token."<br/>";
+$folder = $box_net->folder(0); 
+var_dump($folder->file);
+?>
